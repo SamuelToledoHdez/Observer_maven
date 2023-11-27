8d527b2aee9ff00c2e0a47a2ce9a850aad9708c6
FrancescoMarelli <alu0101161730@ull.edu.es>
2023-11-27 19:03:15 +0000

    Ventana emergente noticia

--- src/main/java/GUI/NewsGUI.java
+++ src/main/java/GUI/NewsGUI.java
@@ -30,8 +30,8 @@ public class NewsGUI extends JFrame {
 
     private JPanel crearPanelPrincipal(String tituloPagina) {
         JPanel panelCabecera = crearPanelCabecera(tituloPagina);
-
         JPanel panelPrincipal = new JPanel(new BorderLayout());
+
         panelCabecera.setBackground(new Color(222, 222, 216));
         panelCabecera.setOpaque(true);
 
@@ -60,26 +60,12 @@ public class NewsGUI extends JFrame {
         panelNoticias.setLayout(new BoxLayout(panelNoticias, BoxLayout.Y_AXIS));
         for (String noticia : noticias) {
             JPanel panelTituloNoticia = crearPanelTituloNoticia(noticia);
-
-            // Agregar MouseListener para resaltar al pasar el ratón
-            panelTituloNoticia.addMouseListener(new MouseAdapter() {
-                @Override
-                public void mouseEntered(MouseEvent e) {
-                    panelTituloNoticia.setBackground(new Color(160, 158, 149)); // Cambiar color al pasar el ratón
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    panelTituloNoticia.setBackground(new Color(222, 222, 216)); // Restaurar color al salir del ratón
-                }
-            });
             panelNoticias.add(panelTituloNoticia);
             panelNoticias.add(Box.createRigidArea(new Dimension(5, 10)));
         }
         return panelNoticias;
     }
 
-    // En el método crearPanelTituloNoticia, para establecer el color inicial
     private JPanel crearPanelTituloNoticia(String noticia) {
         String titulo = extraerTituloNoticia(noticia);
 
@@ -87,11 +73,16 @@ public class NewsGUI extends JFrame {
         panelTituloNoticia.setBackground(new Color(222, 222, 216));
         panelTituloNoticia.setBorder(BorderFactory.createLineBorder(new Color(200, 195, 179), 1));
 
+        JLabel labelTituloNoticia = new JLabel(titulo);
+        configurarEstiloLabelTitulo(labelTituloNoticia, noticia);
+        labelTituloNoticia.addMouseListener(new NoticiaMouseListener(noticia, titulo)); // Lanza noticia
+        panelTituloNoticia.addMouseListener(new NoticiaMouseListener(noticia, titulo));
+
         // Agregar MouseListener para resaltar al pasar el ratón
         panelTituloNoticia.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseEntered(MouseEvent e) {
-                panelTituloNoticia.setBackground(new Color(200, 200, 196)); // Cambiar color al pasar el ratón
+                panelTituloNoticia.setBackground(new Color(160, 158, 149)); // Cambiar color al pasar el ratón
             }
 
             @Override
@@ -99,9 +90,6 @@ public class NewsGUI extends JFrame {
                 panelTituloNoticia.setBackground(new Color(222, 222, 216)); // Restaurar color al salir del ratón
             }
         });
-
-        JLabel labelTituloNoticia = new JLabel(titulo);
-        configurarEstiloLabelTitulo(labelTituloNoticia, noticia);
 
         panelTituloNoticia.add(labelTituloNoticia);
         return panelTituloNoticia;
@@ -138,6 +126,61 @@ public class NewsGUI extends JFrame {
         // Si no hay coincidencia, devolver toda la primera línea (o toda la noticia si no hay línea)
         String[] lineas = noticia.split("\n");
         return lineas.length > 0 ? lineas[0].trim() : noticia.trim();
+    }
+
+    private static class NoticiaMouseListener extends MouseAdapter {
+        final private String noticia;
+        final private String tituloNoticia;
+
+        public NoticiaMouseListener(String noticia, String tituloNoticia) {
+            this.noticia = noticia;
+            this.tituloNoticia = tituloNoticia;
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // Crear una nueva ventana para mostrar la noticia completa
+            JFrame ventanaNoticia = new JFrame(tituloNoticia);
+            ventanaNoticia.setIconImage(new ImageIcon("nalogo.png").getImage());
+            ventanaNoticia.setSize(600, 400);
+            ventanaNoticia.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // Cerrar solo la ventana actual al cerrar
+
+            // Crear un panel principal con BorderLayout
+            JPanel panelPrincipal = new JPanel(new BorderLayout());
+
+            // Crear un área de texto estilizada para la noticia
+            JLabel labelTituloNoticia = new JLabel("<html><div style='width: 400px; text-align: center;'>" + tituloNoticia + "</div></html>");
+            labelTituloNoticia.setFont(new Font("Roboto", Font.BOLD, 18)); // Ajustar la fuente y el tamaño
+            labelTituloNoticia.setBackground(new Color(222, 222, 216)); // Color de fondo suave
+            labelTituloNoticia.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10)); // Ajustar el margen interno
+            labelTituloNoticia.setOpaque(true); // Hacer visible el fondo
+            labelTituloNoticia.setPreferredSize(new Dimension(400, labelTituloNoticia.getPreferredSize().height)); // Limitar el ancho
+            panelPrincipal.add(labelTituloNoticia, BorderLayout.NORTH);
+
+            JTextArea textoNoticia = new JTextArea(noticia);
+            textoNoticia.setLineWrap(true);
+            textoNoticia.setWrapStyleWord(true);
+            textoNoticia.setEditable(false);
+            textoNoticia.setBackground(new Color(220, 220, 216));
+            textoNoticia.setFont(new Font("Roboto", Font.PLAIN, 16)); // Ajustar la fuente y el tamaño
+
+            // Agregar el área de texto a un JScrollPane para permitir el desplazamiento
+            JScrollPane scrollPane = new JScrollPane(textoNoticia);
+            scrollPane.setBorder(BorderFactory.createEmptyBorder()); // Eliminar el borde del JScrollPane
+            panelPrincipal.add(scrollPane, BorderLayout.CENTER);
+
+            // Configurar el fondo y el título resaltado
+            panelPrincipal.setBackground(new Color(222, 222, 216)); // Color de fondo suave
+            textoNoticia.setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createLineBorder(new Color(149, 148, 138), 2), // Borde resaltado
+                    BorderFactory.createEmptyBorder(10, 10, 10, 10) // Margen interno
+            ));
+
+            // Agregar el panel principal a la ventana de la noticia
+            ventanaNoticia.add(panelPrincipal);
+            ventanaNoticia.setVisible(true);
+        }
+
     }
 
 
