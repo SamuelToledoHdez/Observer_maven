d79c87095581f4ab52587e424f25d94b89261db0
sami <stolh1412@gmail.com>
2023-11-30 16:24:40 +0000

    Constructor

--- src/main/java/org/app/Categoria.java
+++ src/main/java/org/app/Categoria.java
@@ -1,6 +1,10 @@
 package org.app;
 
 public abstract class Categoria {
+
+    Categoria(String categoria){
+        cat = categoria;
+    }
     public String getCat() {
         return cat;
     }
--- src/main/java/org/app/Motor.java
+++ src/main/java/org/app/Motor.java
@@ -1,4 +1,5 @@
 package org.app;
 
 public class Motor {
+
 }
